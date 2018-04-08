680ec233396a2b312616514e1df8c4593c0d98c0
nolto <skatl0523@gmail.com>
2018-04-08 17:32:23 +0900

    리팩토링? 약간

--- ch07/src/blackJack2/Main.java
+++ ch07/src/blackJack2/Main.java
@@ -4,14 +4,19 @@ import java.util.Scanner;
 import static blackJack2.Message.*;
 
 public class Main{
-	public static void main(String args[]){
+	private static int INIT_RECEIVE_CARD_COUNT	=2;
+	private static int DEALER_BURST				=1;
+	private static int DEALER_PANELTY_DRAW		=-1;
+	private static int DEALER_COMMON_STATE		=0;
+	
+	public static void main(String args[]){		
 		Deck all_card = new Deck();		
 		Dealer D = new Dealer();
 		Gamer G = new Gamer();
 		Rule rules = new Rule(D,G);
 		Scanner sc = new Scanner(System.in);
 		
-		for(int i=0;i<2;i++) {
+		for(int i=0;i<INIT_RECEIVE_CARD_COUNT;i++) {
 			D.Draw(all_card);
 			G.Draw(all_card);
 		}
@@ -20,7 +25,8 @@ public class Main{
 		
 		String select ="n";
 		boolean playerBurst=false;
-		boolean DealerBurst=false;
+		byte DealerState=0;
+		
 		while(true) {	// Gamer
 			System.out.print(DRAW_INTENTION_MSG);
 			select = sc.next();
@@ -38,32 +44,29 @@ public class Main{
 				continue;
 			}
 		}
-		int mode;
+		
 		while(playerBurst!=true) {
-			mode = rules.Dealer_burst_Check();
-			if(mode==1) {
-				DealerBurst=true;
+			DealerState = rules.Dealer_burst_Check();
+			if(DealerState==DEALER_BURST) {
+				System.out.println(DEALER_BURST_MSG);
 				break;
-			}else if(mode==-1) {
+			}else if(DealerState==DEALER_PANELTY_DRAW) {//딜러는 16점 이하면서 가진 패가 2장이하일 때 1번 더 드로우한다.
 				System.out.println(DEALER_RULE_MSG);
 				D.Draw(all_card);
-			}else if(mode==0) {
+			}else if(DealerState==DEALER_COMMON_STATE) {
 				System.out.print(DRAW_INTENTION_DEALER_MSG);
 				select = sc.next();
 				if(select.equals("y") || select.equals("Y")) {
 					D.Draw(all_card);
-					if(rules.Dealer_burst_Check()==1) {
-						DealerBurst=true;
-						break;
-					}
 				}else if(select.equals("n") || select.equals("N")){
 					break;
 				}else {
 					System.out.println(WRONG_INPUT_MSG);
+					continue;
 				}
 			}
 			D.check_the_card();
 		}
-		rules.WhoIsWin(playerBurst,DealerBurst);
+		rules.WhoIsWin(playerBurst,DealerState);
 	}
 }
--- ch07/src/blackJack2/Rule.java
+++ ch07/src/blackJack2/Rule.java
@@ -21,21 +21,20 @@ public class Rule {
 		}
 		return false;
 	}
-	public int Dealer_burst_Check() {
+	public byte Dealer_burst_Check() {
 		int check_sum=D.check_sum();
-		if(check_sum>BLACK_JACK) {
-			System.out.println(DEALER_BURST_MSG);
+		if(check_sum>BLACK_JACK) {			
 			return 1;
 		}else if(check_sum<=DEALER_PENALTY_SCORE && D.d_card.size()<=DEALER_PENALTY_CARD_COUNT) {	
 			return -1;
 		}
 		return 0;
 	}
-	public void WhoIsWin(boolean PlayerContinue,boolean DealerContinue) {
+	public void WhoIsWin(boolean PlayerContinue,byte DealerContinue) {
 		int gamerSum	= G.check_sum();
 		int dealerSum	= D.check_sum();
 		System.out.printf(PLAYERS_FINAL_SCORE_MSG,dealerSum,gamerSum);
-		if((PlayerContinue!=true) && (DealerContinue!=true)) {
+		if((PlayerContinue!=true) && (DealerContinue!=1)) {
 			if(gamerSum>dealerSum) {
 				System.out.println(GAMER_WIN_MSG);
 			}else if(gamerSum<dealerSum) {
