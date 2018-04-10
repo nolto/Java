13d5c1409d7d73c856217c07344fe0e1bba2f3bc
fffffhaaaiyaaa <skatl0523@gmail.com>
2018-04-10 09:04:40 +0900

    굿

--- ch07/src/ch07/RepairableTest.java
+++ ch07/src/ch07/RepairableTest.java
@@ -7,12 +7,15 @@ public class RepairableTest {
 		
 		Marine marine = new Marine();
 		SCV scv = new SCV();
+		SCV scv2 = new SCV();
 		Medic medic = new Medic();
 		Firebat firebat = new Firebat();
 		
 		scv.repair(tank);			// SCV가 Tank를 수리하도록 한다.
 		scv.repair(dropship);
 //		scv.repair(marine);			// repair(Repairable) in SCV cannot be applied to (Marine)
+		scv.repair(scv);
+		scv.repair(scv2);
 		
 		medic.rescure(firebat);
 	}
@@ -84,13 +87,17 @@ class SCV extends GroundUnit implements Repairable,Rescureable{
 	}
 	
 	void repair(Repairable r) {
-		if(r instanceof Unit) {
-			Unit u = (Unit)r;
-			while(u.hitPoint!=u.MAX_HP) {
-				/* Unit의 HP를 증가시킨다. */
-				u.hitPoint++;
+		if(!this.equals(r)) {		// 주소값을 비교하여 자가수리를 할 수 없도록 처리
+			if(r instanceof Unit) {
+				Unit u = (Unit)r;
+				while(u.hitPoint!=u.MAX_HP) {
+					/* Unit의 HP를 증가시킨다. */
+					u.hitPoint++;
+				}
+				System.out.println(u.toString()+"의 수리가 끝났습니다.");
 			}
-			System.out.println(u.toString()+"의 수리가 끝났습니다.");
+		}else {
+			System.out.println("본인을 수리할 수 없습니다.");
 		}
 	}
 	public String toString() {
