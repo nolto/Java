637bdb17ff0ff314f247b2287f23926005e31286
fffffhaaaiyaaa <skatl0523@gmail.com>
2018-04-11 15:19:59 +0900

    d

--- ch07/src/ch07/InnerEx1.java
+++ ch07/src/ch07/InnerEx1.java
@@ -1,7 +1,7 @@
 package ch07;
-
+/// 중요하지 않은듯하니 넘어가자(익명클래스로 대체했다고 함)
 public class InnerEx1 {
-	class InstanceInner{
+	class InstanceInner{			// 하나의 지역변수 혹은 지역메서드로 생각하면 될듯
 		int iv = 100;
 //		static int cv = 100;			// static변수를 선언할 수 없다.
 		final static int CONST = 100;	// final static은 상수이므로 허용
@@ -23,5 +23,8 @@ public class InnerEx1 {
 	public static void main(String args[]) {
 		System.out.println(InstanceInner.CONST);		// 외부에서 내부 클래스를 사용하려면 final+static이어야 사용 가능한 듯
 		System.out.println(StaticInner.cv);
+		InnerEx1 hi = new InnerEx1();
+//		InstanceInner hello = hi.new InstanceInner();
+//		System.out.println(hello.iv);
 	}
 }
